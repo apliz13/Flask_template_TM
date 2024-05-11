79ed014716f30483d085418eca4decf8824037b1
Alizée Petrini <106526691+apliz13@users.noreply.github.com>
2024-05-11 19:38:58 +0200

    déployer

--- app/config_ex.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY="4a3158a5a908ff71d94a042b1102a089"
-# cette clé est une chaine de 32 caractères aléatoires, elle est utilisée pour signer les cookies de session
-# elle doit être gardée secrète et ne doit pas être partagée    
-# cette valeur est érronée
-# on peut générer une nouvelle clé en utilisant la commande suivante:python3 -c 'import os; print(os.urandom(16).hex())'
-DATABASE= "APPLIZZY.db"
-#ici on définit le nom de la base de données SQLite qui sera utilisée par l'application
--- app/static/js/index_todo.js
+++
@@ -1,8 +0,0 @@
-const cards = document.querySelectorAll('.todo');
-
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        card.classList.toggle('completed');
-    });
-}   );
-
